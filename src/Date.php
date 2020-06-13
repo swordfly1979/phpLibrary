2153d6e36e38b41a7fc1c9fc281601e4315a84ca
swordfly1979 <7115670@qq.com>
2020-06-13 19:38:25 +0800

    Date日期处理类修改

--- src/Date.php
+++ src/Date.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Class name Date.php
  * Created by PhpStorm.
@@ -6,7 +7,7 @@
  * Date: 2020/2/20
  */
 
-namespace swordfly1979\lib;
+namespace swordfly1979;
 
 class Date
 {
@@ -16,27 +17,20 @@ class Date
      * @param $startDate (string|int)  指定开始时间,字符串或时间戳
      * @param $endDate (string|int)    指定结束时间，字符串或时间戳
      * @param $type        类型，day 天，month 月份，quarter 季度，year 年份
-     * @param $returnType  返回日期格式 1(默认)返回字符串'Y-m-d H:i:s' o为返回时间戳
+     * @param $format string  返回日期格式(默认'Y-m-d H:i:s'）timestamp为返回时间戳
      * @return array
      */
 
-    static public function DateArr($startDate = '', $endDate = '', $type = '', $returnType = '1')
+    static public function DateArr($startDate = '', $endDate = '', $type = 'day', $format = 'Y-m-d H:i:s')
     {
-        $str = 'Y-m-d H:i:s';
-        if (is_int($startDate) && !(new self())->isTimeStamp($startDate)) {
+        if (self::checkDate($startDate) == false) {
             return '开始日期格式不正确';
         }
-        if (!is_int($startDate) && !(new self())->isTimeStamp(strtotime($startDate))) {
-            return '开始日期格式不正确';
+        if (self::checkDate($endDate) == false) {
+            return '结束日期格式不正确';
         }
         if (!is_int($startDate)) {
             $startDate = strtotime($startDate);
-        }
-        if (is_int($endDate) && !(new self())->isTimeStamp($endDate)) {
-            return '结束日期格式不正确';
-        }
-        if (!is_int($endDate) && !(new self())->isTimeStamp(strtotime($endDate))) {
-            return '结束日期格式不正确';
         }
         if (!is_int($endDate)) {
             $endDate = strtotime($endDate);
@@ -47,10 +41,10 @@ class Date
         if ($type == 'day') {    // 查询所有日期
             while ($tempDate < $endDate) {
                 $tempDate = strtotime('+' . $i . ' day', $startDate);
-                if ($returnType === 0) {
+                if ($format == 'timestamp') {
                     $returnData[] = $tempDate;
                 } else {
-                    $returnData[] = date($str, $tempDate);
+                    $returnData[] = date($format, $tempDate);
                 }
                 $i++;
             }
@@ -62,7 +56,7 @@ class Date
                 $temp['start'] = date('Y-m-01 00:00:00', $month);
                 $temp['end'] = date('Y-m-t 23:59:59', $month);
                 $tempDate = strtotime($temp['end']);
-                if ($returnType == 0) {
+                if ($format == 'timestamp') {
                     $temp['start'] = strtotime($temp['start']);
                     $temp['end'] = strtotime($temp['end']);
                 }
@@ -70,25 +64,26 @@ class Date
                 $i++;
             }
         } elseif ($type == 'quarter') {    // 查询所有季度以及开始结束时间
-            while (strtotime($tempDate) < strtotime($endDate)) {
+            while ($tempDate < $endDate) {
                 $temp = [];
-                $quarter = strtotime('+' . $i . ' month', strtotime($startDate));
+                $quarter = strtotime('+' . $i . ' month', $startDate);
                 $q = ceil(date('n', $quarter) / 3);
-                $temp['name'] = date('Y', $quarter) . '第' . $q . '季度';
+                $temp['year'] = date('Y', $quarter);
+                $temp['quarter'] = $q;
                 $temp['startDate'] = date('Y-m-01', mktime(0, 0, 0, $q * 3 - 3 + 1, 1, date('Y', $quarter)));
                 $temp['endDate'] = date('Y-m-t', mktime(23, 59, 59, $q * 3, 1, date('Y', $quarter)));
-                $tempDate = $temp['endDate'];
+                $tempDate = strtotime($temp['endDate']);
                 $returnData[] = $temp;
                 $i = $i + 3;
             }
         } elseif ($type == 'year') {    // 查询所有年份以及开始结束时间
-            while (strtotime($tempDate) < strtotime($endDate)) {
+            while ($tempDate < $endDate) {
                 $temp = [];
-                $year = strtotime('+' . $i . ' year', strtotime($startDate));
+                $year = strtotime('+' . $i . ' year', $startDate);
                 $temp['name'] = date('Y', $year) . '年';
                 $temp['startDate'] = date('Y-01-01', $year);
                 $temp['endDate'] = date('Y-12-31', $year);
-                $tempDate = $temp['endDate'];
+                $tempDate = strtotime($temp['endDate']);
                 $returnData[] = $temp;
                 $i++;
             }
@@ -108,13 +103,11 @@ class Date
 
     static public function dateIncDec($date = '', $type = 'day', $number = 1, $format = 'y-m-d')
     {
-        if (is_int($date) && !(new self())->isTimeStamp($date)) {
+        $ret = self::checkDate($date);
+        if ($ret === false) {
             return '日期格式不正确';
         }
-        if (!is_int($date) && !(new self())->isTimeStamp(strtotime($date))) {
-            return '日期格式不正确';
-        }
-        if (!is_int($date)) {
+        if ($ret == 'str') {
             $date = strtotime($date);
         }
         $returnData = [];
@@ -130,7 +123,50 @@ class Date
             $i++;
         }
         return $returnData;
+    }
 
+    /**
+     * 返回指定天的起始、结束时间戳
+     *
+     * @param $day (string|int)  指定的天,字符串或时间戳
+     */
+    static public function timeStamp($day = '')
+    {
+        $return['error'] = 0;
+        if (empty($day)) $day = time();
+        $ret = self::checkDate($day);
+        if ($ret == false) {
+            $return['error'] = 1;
+            $return['info'] = '日期格式不正确:' . $day;
+        }
+        if ($return['error'] == 1) return $return;
+        if ($ret == 'str') {
+            $day = strtotime($day);
+        }
+        $startTime = strtotime(date("Y-m-d", $day));
+        $endTime = $startTime + 60 * 60 * 24;
+        return [$startTime, $endTime];
+    }
+
+    /*
+     * 是否正确的时间格式
+     * @param $date 要验证的时间（时间戳或时间字符串）
+     * return bool false 时间格式错误; string int 原时间为时间戳 string str 原时间为字符串格式
+     */
+    static public function checkDate($date = null)
+    {
+        if (!$date) return false;
+        if (is_int($date) && !(new self())->isTimeStamp($date)) {
+            return false;
+        }
+        if (!is_int($date) && !(new self())->isTimeStamp(strtotime($date))) {
+            return false;
+        }
+        if (is_int($date)) {
+            return 'int';
+        } else {
+            return 'str';
+        }
     }
 
     /**
